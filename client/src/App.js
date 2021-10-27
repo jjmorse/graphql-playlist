969d3bcbd9db9f4d3dcf7b4d3b5f3730367091b9
Josh Morse <jjmorse@gmail.com>
2021-10-27 08:30:13 -0600

    Finished step 23

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+//components
+import BookList from './components/BookList';
+
+function App() {
+  return (
+    <div id="main">
+      <h1>Reading List</h1>
+      <BookList></BookList>
+    </div>
+  );
+}
+
+export default App;
