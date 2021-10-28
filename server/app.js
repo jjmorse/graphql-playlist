ee001965d0f75e1ec6b3741db751fcf054598648
Josh Morse <jjmorse@gmail.com>
2021-10-28 09:46:56 -0600

    Through step 28

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect('mongodb+srv://admin:Pk0TbuGzlaptMG2h@cluster0.mg6jf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 mongoose.connection.once('open', () => {
